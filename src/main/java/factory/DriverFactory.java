6883c81783b30ddc6cb18d734242d83ea645c5bd
Sai <Sain@gmail.com>
2026-09-04 17:27:02 +0530

    Removing unwanted code

--- src/main/java/factory/DriverFactory.java
+++ src/main/java/factory/DriverFactory.java
@@ -2,7 +2,6 @@ package factory;
 
 import java.net.MalformedURLException;
 import java.net.URI;
-sai narasimha
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
